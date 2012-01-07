9448434eafeb7af8fb548b3ca27e769f42161c19
Dustin Sallings <dustin@spy.net>
2012-01-07 15:08:30 -0800

    Return the groups in folded alphabetical order by full name.

--- groups.go
+++ groups.go
@@ -1,13 +1,34 @@
 package yammer
 
+import (
+	"sort"
+	"strings"
+)
+
+type groupSlice []Group
+
+func (g groupSlice) Len() int {
+	return len(g)
+}
+
+func (g groupSlice) Less(i, j int) bool {
+	return strings.ToLower(g[i].FullName) < strings.ToLower(g[j].FullName)
+}
+
+func (g groupSlice) Swap(i, j int) {
+	g[i], g[j] = g[j], g[i]
+}
+
 // Get the full list of users.
 func (c *Client) ListGroups() ([]Group, error) {
 	u := "https://www.yammer.com/api/v1/groups.json"
 
-	rv := make([]Group, 0)
+	rv := make(groupSlice, 0)
 	if err := decodeReq(c, u, &rv, map[string]string{}); err != nil {
 		return rv, err
 	}
 
-	return rv, nil
+	sort.Sort(rv)
+
+	return []Group(rv), nil
 }
